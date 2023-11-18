f6500d69da9c623f939794d01ac501028b1aea7a
herexiong <xiongshengjunchina@gmail.com>
2023-11-18 13:00:21 +0800

    V2.1 完善了二维数组的操作函数

--- TwoDimen_array.c
+++ TwoDimen_array.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
-char col_data[2][20];
+#include <stdlib.h>
+volatile char col_data[2][20];
 
 char *hello ="hello world\n";
 
-char* getData(int col){
-	return *(col_data+col);	
+char* getData(volatile char dataGroup[2][20],int col){//读数据
+   return *(dataGroup+col);
 }
 
-void pushData(int col,char *data){
-	int i=0;
-	while(*data!= '\n'){
-		col_data[col][i]=*data;
-		data++;
-		i++;
-	}
+void pushData(volatile char dataGroup[2][20],int col,char *data){//写数据
+   int i=0;
+   memset(*(dataGroup+col),'\0',sizeof(*(dataGroup+col)));//写数据前清除上次写入的数据
+   while(*data!= '\0'){
+      dataGroup[col][i]=*data;
+      data++;
+      i++;
+   }
 }
 
 int main()
 {
    /*  Write C code in this online editor and run it. */
-   pushData(0 , hello);
-   pushData(1 , "...");
-   pushData(2 , "fuck");	
-   printf("%s\n",getData(0));
-   printf("%s\n",getData(1));
-   printf("%s\n",getData(2));
+   pushData(&col_data[2][20], 0 , hello);
+   pushData(&col_data[2][20], 1 , "...");
+   pushData(&col_data[2][20], 2 , "fuck");	
+   printf("%s\n",getData(&col_data[2][20], 0));
+   printf("%s\n",getData(&col_data[2][20], 1));
+   printf("%s\n",getData(&col_data[2][20], 2));
    return 0;
 }
-
